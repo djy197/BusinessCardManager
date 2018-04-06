40225a581678b73a22e20d15c480cf7882585c82
djy <1085651864@qq.com>
2018-04-06 16:54:21 +0800

    version 0.99

--- app/src/main/java/com/bcm/djy/businesscardmanager/ManageActivity.java
+++ app/src/main/java/com/bcm/djy/businesscardmanager/ManageActivity.java
@@ -6,6 +6,8 @@ import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
@@ -24,46 +26,21 @@ public class ManageActivity extends AppCompatActivity {
     private SQLiteDatabase database = null;
     private SimpleAdapter simAdapt;
     private ListView listView;
-    private List<Map<String, String>> data = new ArrayList<Map<String, String>>();
+    private List<Map<String, Object>> data = new ArrayList<Map<String, Object>>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage);
 
-        myHelper = new MyHelper(this);
-        database = myHelper.getWritableDatabase();
-
-        Cursor cursor = database.query(DatabaseStatic.TABLE_NAME, null, null, null, null, null, DatabaseStatic.NAME);
-        if(cursor.moveToFirst()) // 显示数据库的内容
-        {
-            for(; !cursor.isAfterLast(); cursor.moveToNext()) // 获取查询游标中的数据
-            {
-                Map<String, String> item = new HashMap<String, String>();
-                item.put("com", cursor.getString(cursor.getColumnIndex(DatabaseStatic.COM)));
-                item.put("name", cursor.getString(cursor.getColumnIndex(DatabaseStatic.NAME)));
-                item.put("tel", cursor.getString(cursor.getColumnIndex(DatabaseStatic.TEL)));
-                item.put("email", cursor.getString(cursor.getColumnIndex(DatabaseStatic.EMAIL)));
-                data.add(item);
-            }
-        }
-        cursor.close();
-
-        if(data.isEmpty())
-            Toast.makeText(this, "数据库为空", Toast.LENGTH_SHORT).show();
-
-        listView = (ListView) findViewById(R.id.listView);
-
-        simAdapt = new SimpleAdapter(
-                this,
-                data,
-                R.layout.item_layout,
-                new String[] { "com", "name", "tel", "email" },// 与下面数组元素要一一对应
-                new int[] { R.id.item_company, R.id.item_name, R.id.item_tel, R.id.item_email });
-
-        listView.setAdapter(simAdapt);
+        listChange();
     }
 
+    @Override
+    public void onResume(){
+        super.onResume();
+        listChange();
+    }
 
     public void btn_index(View view) {
         startActivity(new Intent(ManageActivity.this,IndexActivity.class));
@@ -81,5 +58,71 @@ public class ManageActivity extends AppCompatActivity {
 
     public void btn_add(View view) {
         startActivity(new Intent(ManageActivity.this,DetailActivity.class));
+    }
+
+    public void btn_delete(View view) {
+        myHelper = new MyHelper(this);
+        database = myHelper.getWritableDatabase();
+        for(int i=0;i<data.size();i++)
+            if(data.get(i).get("state")=="checked")
+                database.delete(DatabaseStatic.TABLE_NAME, DatabaseStatic.ID + "= ?",new String[]{data.get(i).get("id").toString()});
+        listChange();
+    }
+
+    public void listChange(){
+        data = new ArrayList<Map<String, Object>>();
+        myHelper = new MyHelper(this);
+        database = myHelper.getWritableDatabase();
+
+        Cursor cursor = database.query(DatabaseStatic.TABLE_NAME, null, null, null, null, null, DatabaseStatic.NAME);
+        if(cursor.moveToFirst()) // 显示数据库的内容
+        {
+            for(; !cursor.isAfterLast(); cursor.moveToNext()) // 获取查询游标中的数据
+            {
+                Map<String, Object> item = new HashMap<String, Object>();
+                item.put("com", cursor.getString(cursor.getColumnIndex(DatabaseStatic.COM)));
+                item.put("name", cursor.getString(cursor.getColumnIndex(DatabaseStatic.NAME)));
+                item.put("tel", cursor.getString(cursor.getColumnIndex(DatabaseStatic.TEL)));
+                item.put("email", cursor.getString(cursor.getColumnIndex(DatabaseStatic.EMAIL)));
+                item.put("id",cursor.getString(cursor.getColumnIndex(DatabaseStatic.ID)));
+                item.put("state","unchecked");
+                item.put("img",R.drawable.unchecked);
+                data.add(item);
+            }
+        }
+        cursor.close();
+
+        if(data.isEmpty())
+            Toast.makeText(this, "数据库为空", Toast.LENGTH_SHORT).show();
+
+        listView = (ListView) findViewById(R.id.listView);
+
+        simAdapt = new SimpleAdapter(
+                this,
+                data,
+                R.layout.item_select_layout,
+                new String[] { "com", "name", "tel", "email", "img" },// 与下面数组元素要一一对应
+                new int[] { R.id.item_company, R.id.item_name, R.id.item_tel, R.id.item_email, R.id.item_check });
+
+        listView.setAdapter(simAdapt);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Map<String, Object> item = data.get(position);
+                if(item.get("state")=="unchecked")
+                {
+                    item.put("img",R.drawable.checked);
+                    item.put("state","checked");
+                }
+                else if(item.get("state")=="checked")
+                {
+                    item.put("img",R.drawable.unchecked);
+                    item.put("state","uncheck");
+                }
+                data.remove(position);//清除此行对应数据集中的数据
+                data.add(position, item);
+                simAdapt.notifyDataSetChanged();
+            }
+        });
     }
 }
